db82c3457599b641dac23df41ae2028389c41c97
SunEom <dkxl0701@gmail.com>
2024-04-23 15:39:19 +0900

    프로젝트 초기화

---
+++ Cleset/ClesetApp.swift
@@ -0,0 +1,30 @@
+//
+//  ClesetApp.swift
+//  Cleset
+//
+//  Created by 엄태양 on 4/23/24.
+//
+
+import SwiftUI
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
+@main
+struct YourApp: App {
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
+    }
+  }
+}
---
+++ Cleset/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cleset
+//
+//  Created by 엄태양 on 4/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
